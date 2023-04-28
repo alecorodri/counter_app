9736ea8fbbf6fdf82553012d9688359fb1a4d78a
alecorodri <aleco.rodri@gmail.com>
2023-04-28 00:30:07 -0400

    React Hello World ;)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+function App() {
+    //document.createElemente
+    return <h1>Hola Mundo</h1>
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 )
