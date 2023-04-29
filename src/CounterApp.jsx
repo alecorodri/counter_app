e053f7a7a3c99727f64b3eca73d3b41143ce81c5
alecorodri <aleco.rodri@gmail.com>
2023-04-29 15:22:52 -0400

    handleSubst and handleReset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -6,21 +6,29 @@ import PropTypes from 'prop-types'
 
 export const CounterApp = ({ value }) => {
 
-    const [counter, setCounter] = useState( value )
+    const [counter, setCounter] = useState(value)
 
-    const handAdd = (event, newValue) => {
+    const handleAdd = (event, newValue) => {
         setCounter(counter + 1);
         //setCounter((c)=> c+1);
     }
+
+    const handleSubst = (event, newValue) => {
+        setCounter(counter - 1);
+    }
+
+    const handleReset = (event, newValue) => {
+        setCounter(value);
+    }
+
 
     return (
         <>
             <h1> Counter App </h1>
             <h2> {counter} </h2>
-            <button onClick={ handAdd }>
-                +1
-
-            </button>
+            <button onClick={handleAdd}>+1</button>
+            <button onClick={handleSubst}>-1</button>
+            <button onClick={handleReset}>Reset</button>
 
         </>
 
